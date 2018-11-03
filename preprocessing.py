1e5cfab44ece04e84ff2c1b86661f8b7eccbaaa9
Beatriz Rivera <rivers.tris@gmail.com>
2018-11-03 13:38:14 +0800

    added load_csv function

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,17 @@
 import csv
 
-# loadDataset()
-f = open('data/sample.csv', 'r')
-d = f.read()
-dataset = d.split('\n')
-
-# loadTagset()
-g = open('data/tags.csv', 'r')
-e = g.read()
-tagset = e.split('\n')
+# loadDataset() and loadTagset()
+def load_csv(filename):
+	f = open(filename, 'r')
+	d = f.read()
+	data = d.split('\n')
+	return data
 
 # printDataset()
 def print_list(input_list):
 	print('\n'.join(map(str, input_list)))
+
+dataset = load_csv('data/sample.csv')
+tagset = load_csv('data/tags.csv')
 
 print_list(tagset)
